58ca148ab36c3362a55ab567058b83080e79c91c
Rok Rozman <rok.rozman2@student.um.si>
2023-02-28 12:18:07 +0100

    missing data is NaN

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -11,16 +11,28 @@ print(data_dict)
 for i in range(len(data_dict)):
     temp=data_dict['json'][i]
     json_object = json.loads(temp)
+    if i == 0:
+        print(json_object['arsopodatki']['postaja'][14]['no2'])
+        if(json_object['arsopodatki']['postaja'][14]['no2'] == ''):
+            print('ok')
+        else:
+            print('ne')
+        print(type(json_object['arsopodatki']['postaja'][14]['no2']))
+
     # print(json_object['arsopodatki']['postaja'][14])
+
     d.append(
         {
-            'no2': json_object['arsopodatki']['postaja'][14]['no2'],
-            'pm2.5': json_object['arsopodatki']['postaja'][14]['pm2.5'],
-            'datum_od': json_object['arsopodatki']['postaja'][14]['datum_od'],
-            'o3': json_object['arsopodatki']['postaja'][14]['o3'],
-            'pm10': json_object['arsopodatki']['postaja'][14]['pm10'],
-            'datum_do': json_object['arsopodatki']['postaja'][14]['datum_do']
+            'no2': json_object['arsopodatki']['postaja'][14]['no2'] if not json_object['arsopodatki']['postaja'][14]['no2'] == '' else None,
+            'pm2.5': json_object['arsopodatki']['postaja'][14]['pm2.5'] if not json_object['arsopodatki']['postaja'][14]['pm2.5'] == '' else None,
+            'datum_od': json_object['arsopodatki']['postaja'][14]['datum_od'] if not json_object['arsopodatki']['postaja'][14]['datum_od'] == '' else None,
+            'o3': json_object['arsopodatki']['postaja'][14]['o3'] if not json_object['arsopodatki']['postaja'][14]['o3'] == '' else None,
+            'pm10': json_object['arsopodatki']['postaja'][14]['pm10'] if not json_object['arsopodatki']['postaja'][14]['pm10'] == '' else None,
+            'datum_do': json_object['arsopodatki']['postaja'][14]['datum_do'] if not json_object['arsopodatki']['postaja'][14]['datum_do'] == '' else None
         })
 
 df = pd.DataFrame(d)
 print(df)
+
+# missing data
+print(df.isnull().sum())
