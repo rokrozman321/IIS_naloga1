8fe7a0e3ed35e9634998059c71c6b9bb8cea5db7
Rok Rozman <rok.rozman2@student.um.si>
2023-03-01 23:12:25 +0100

    update

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -1,9 +1,9 @@
-import pandas as pd
 import json
 import numpy as np
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 import os
+import pandas as pd
 
 
 def process_data():
