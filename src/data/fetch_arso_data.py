c974a740f1b29a520bc06d1d874fd2c0397d90a3
Rok Rozman <rok.rozman2@student.um.si>
2023-03-21 09:53:40 +0100

    dodano za github actions to get arso data

---
+++ src/data/fetch_arso_data.py
@@ -0,0 +1,23 @@
+import json
+import requests
+import os
+import time
+
+def fetch_data():
+    print("Fetch arso data")
+    # kje si bomo shranili raw podatke
+    filename = "data/raw/data.json" 
+
+    # ustvarimo folder in datoteko ce se ne obstaja
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+    # naredimo request
+    file = requests.get('https://arsoxmlwrapper.app.grega.xyz/api/air/archive')
+    # pridobimo json podatke
+    podatki = file.json()
+
+    # zapisemo podatke v datoteko
+    with open(filename, 'w') as f:
+        json.dump(podatki, f)
+
+fetch_data()
