e539b8dcf831213349e7da19e8441038d50b56e7
Rok Rozman <rok.rozman2@student.um.si>
2023-03-29 14:42:00 +0200

    s python script curr to ref

---
+++ src/data/curr_to_ref.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def curr_to_ref():
+    df = pd.read_csv('data/processed/current_data.csv')
+    df1 = pd.read_csv('data/processed/reference_data.csv')
+    print(df1)
+    df.to_csv('data/processed/reference_data.csv', index=False)
+    print("Po kopiranju")
+
+    df1 = pd.read_csv('data/processed/reference_data.csv')
+    print(df1)
+
+curr_to_ref()
