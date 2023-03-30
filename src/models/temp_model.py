bfb9d023a44d9bfa3a121f0c4fd6838231c8e898
Rok Rozman <rok.rozman2@student.um.si>
2023-03-30 11:57:46 +0200

    fix to log model metric

--- src/models/temp_model.py
+++ src/models/temp_model.py
@@ -29,7 +29,7 @@ def train_model2():
     mlflow.set_experiment("IIS1")
 
     # enable autologging
-    mlflow.sklearn.autolog()
+    mlflow.sklearn.autolog(exclusive=False)
 
     filename = 'data/processed/train_data.csv'
     df = pd.read_csv(filename)
@@ -57,68 +57,68 @@ def train_model2():
     # print(X_test)
     # print("---------------------------")
     # print(y_test)
+    with mlflow.start_run():
+        reg = LinearRegression()
+        reg.fit(X_train, y_train)
 
-    reg = LinearRegression()
-    reg.fit(X_train, y_train)
+        # show logged data
+        # for key, data in fetch_logged_data(run_id).items():
+        # print(r)
+        # print("\n---------- logged {} ----------".format(run_id.key))
+        # pprint(run_id.data)
 
-    # show logged data
-    # for key, data in fetch_logged_data(run_id).items():
-    # print(r)
-    # print("\n---------- logged {} ----------".format(run_id.key))
-    # pprint(run_id.data)
+        train_pred = reg.predict(X_train)
+        test_pred = reg.predict(X_test)
 
-    train_pred = reg.predict(X_train)
-    test_pred = reg.predict(X_test)
+        # print(reg.score(X_test, y_test))
 
-    # print(reg.score(X_test, y_test))
+        filename = 'models/model2.joblib' 
+        # ustvarimo folder in datoteko ce se ne obstaja
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
 
-    filename = 'models/model2.joblib' 
-    # ustvarimo folder in datoteko ce se ne obstaja
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
+        joblib.dump(reg, filename)
 
-    joblib.dump(reg, filename)
+        filename = 'reports/train_metrics.txt' 
+        # ustvarimo folder in datoteko ce se ne obstaja
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
 
-    filename = 'reports/train_metrics.txt' 
-    # ustvarimo folder in datoteko ce se ne obstaja
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
+        train_mae = mean_absolute_error(y_train, train_pred)
+        print("train mae: ", train_mae)
+        mlflow.log_metric("train_mae", train_mae)
 
-    train_mae = mean_absolute_error(y_train, train_pred)
-    print("train mae: ", train_mae)
-    mlflow.log_metric("train_mae", train_mae)
+        train_mse = mean_squared_error(y_train, train_pred)
+        print("train_mse ", train_mse)
+        mlflow.log_metric("train_mse", train_mse)
 
-    train_mse = mean_squared_error(y_train, train_pred)
-    print("train_mse ", train_mse)
-    mlflow.log_metric("train_mse", train_mse)
+        train_evs = explained_variance_score(y_train, train_pred)
+        print("train_evs", train_evs)
+        mlflow.log_metric("train_evs", train_evs)
 
-    train_evs = explained_variance_score(y_train, train_pred)
-    print("train_evs", train_evs)
-    mlflow.log_metric("train_evs", train_evs)
+        # reports/metrics.txt
+        filename = 'reports/metrics.txt' 
+        # ustvarimo folder in datoteko ce se ne obstaja
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
 
-    # reports/metrics.txt
-    filename = 'reports/metrics.txt' 
-    # ustvarimo folder in datoteko ce se ne obstaja
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
+        test_mae = mean_absolute_error(y_test, test_pred)
+        print("test_mae ", test_mae)
+        mlflow.log_metric("test_mae", test_mae)
 
-    test_mae = mean_absolute_error(y_test, test_pred)
-    print("test_mae ", test_mae)
-    mlflow.log_metric("test_mae", test_mae)
+        test_mse = mean_squared_error(y_test, test_pred)
+        print("test_mse", test_mse)
+        mlflow.log_metric("test_mse", test_mse)
 
-    test_mse = mean_squared_error(y_test, test_pred)
-    print("test_mse", test_mse)
-    mlflow.log_metric("test_mse", test_mse)
+        test_evs = explained_variance_score(y_test, test_pred)
+        print("test_evs: ", test_evs)
+        mlflow.log_metric("test_evs", test_evs)
 
-    test_evs = explained_variance_score(y_test, test_pred)
-    print("test_evs: ", test_evs)
-    mlflow.log_metric("test_evs", test_evs)
+        run_id = mlflow.last_active_run().info.run_id
+        print("Logged data and model in run {}".format(run_id))
+        
+        with open("reports/train_metrics.txt", "w") as f:
+            f.write(f"Train mae: {train_mae:.4f}\nTrain mse: {train_mse:.4f}\nTrain evs: {train_evs:.4f}")
 
-    run_id = mlflow.last_active_run().info.run_id
-    print("Logged data and model in run {}".format(run_id))
-    
-    with open("reports/train_metrics.txt", "w") as f:
-        f.write(f"Train mae: {train_mae:.4f}\nTrain mse: {train_mse:.4f}\nTrain evs: {train_evs:.4f}")
-
-    with open("reports/metrics.txt", "w") as f:
-        f.write(f"Test mae: {test_mae:.4f}\nTest mse: {test_mse:.4f}\nTest evs: {test_evs:.4f}")
+        with open("reports/metrics.txt", "w") as f:
+            f.write(f"Test mae: {test_mae:.4f}\nTest mse: {test_mse:.4f}\nTest evs: {test_evs:.4f}")
 
 
 train_model2()
